5321ee7459284cf81c9eb018c040905ce9c2fb46
calibay <baylife314@gmail.com>
2016-07-25 00:11:43 +0000

    add params in url

---
+++ public/src/converter.js
@@ -0,0 +1,7 @@
+
+
+exports.convert = function convert(value) {
+    var unix = 123;
+    var natural = "123";
+    return {unix: unix, natural: natural};
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express");
+var converter = require('./public/src/converter');
 var app = express();
+var PORT = process.env.PORT;
+var IP = process.env.IP;
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-    res.end();
+app.get('/:value', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(converter.convert(req.params.value));
 });
 
-app.listen(8080, () => {
-    console.log("Server running on port 8080");
+app.listen(PORT, () => {
+    console.log("Server running on " + IP + ":" + PORT);
 });
