71d62469cf62c2aeb5bd1a1f9b307bc35ae00a87
calibay <baylife314@gmail.com>
2016-07-26 01:28:23 +0000

    converts unix time

--- public/src/converter.js
+++ public/src/converter.js
@@ -1,7 +1,33 @@
 
 
 exports.convert = function convert(value) {
-    var unix = 123;
-    var natural = "123";
-    return {unix: unix, natural: natural};
+    var unix = null;
+    var natural = null;
+
+    var monthNames = [
+        "January", "February", "March", 
+        "April", "May", "June",
+        "July", "August", "September", 
+        "October", "November", "December"
+        ];
+    var monthShortNames = [
+        "Jan", "Feb", "Mar", 
+        "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", 
+        "Oct", "Nov", "Dec"
+        ];
+
+    if (/^[0-9]+$/.test(value)) {
+        if (Math.abs(value) <= 2147485547) {
+            unix = value;
+            var date = new Date(value * 1000);
+            var month = monthNames[date.getMonth()];
+            var day = date.getDate();
+            var year = date.getYear() + 1900;
+            natural = month + " " + day + ', ' + year;
+        }
+        
+    }
+
+    return {unix: +unix, natural: natural};
 };
