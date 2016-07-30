aca1b7ad58607f6ca19ed3c9e428a06981273226
calibay <baylife314@gmail.com>
2016-07-30 03:03:45 +0000

    final version

--- public/src/converter.js
+++ public/src/converter.js
@@ -1,8 +1,11 @@
 
 
 exports.convert = function convert(value) {
-    var unix = null;
-    var natural = null;
+    var unix = null,
+    natural = null,
+    month = '',
+    day = '',
+    year = '';
 
     var monthNames = [
         "January", "February", "March", 
@@ -17,17 +20,46 @@ exports.convert = function convert(value) {
         "Oct", "Nov", "Dec"
         ];
 
+    function findMonth(m) {
+        var strToUpper = monthFromValue.charAt(0).toUpperCase() + monthFromValue.slice(1);
+        return (m.includes(strToUpper));
+    }
     if (/^[0-9]+$/.test(value)) {
         if (Math.abs(value) <= 2147485547) {
-            unix = value;
+            unix = +value;
             var date = new Date(value * 1000);
-            var month = monthNames[date.getMonth()];
-            var day = date.getDate();
-            var year = date.getYear() + 1900;
+            month = monthNames[date.getMonth()];
+            day = date.getDate();
+            year = date.getYear() + 1900;
             natural = month + " " + day + ', ' + year;
         }
-        
-    }
+    } else {
+        var monthFromValue = value.match(/^[A-Za-z]+/);
+        if (monthFromValue != null) {
+            monthFromValue = monthFromValue[0];
+            var monthIndex = monthNames.findIndex(findMonth);
+            if (monthIndex != -1) {
+                month = monthNames[monthIndex];
+                value = value.slice(monthFromValue.length);
+                value = value.replace(/ |,/g, "");
+                if (!(value.length > 6) && !(value.length < 5)) {
+                    if (value.length == 6) {
+                        day = value.slice(0,2);
+                    } else if (value.length == 5 ) {
+                        day = value.slice(0,1);
+                    }
+                    year = value.slice(day.length);
+                
+                    natural = month + " " + day + ', ' + year;
+                    unix = Date.parse(natural) / 1000;
+                    console.log(unix);
+                    if (isNaN(unix)) {
+                        natural = null;
+                    }
+                }
+            }            
+        }
 
-    return {unix: +unix, natural: natural};
+    }
+    return {unix: unix, natural: natural };
 };
